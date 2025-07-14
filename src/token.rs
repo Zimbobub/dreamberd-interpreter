f835341631d9343b8924f1d97795d98187184a7b
Zimbobub <89832179+Zimbobub@users.noreply.github.com>
2025-07-14 14:03:59 +1000

    all keywords done

--- src/token.rs
+++ src/token.rs
@@ -1,11 +1,36 @@
+use crate::runner::variables::VariableType;
+
 
 
 
 pub enum Token {
     Keyword(Keyword),
-    Literal
+    Literal(VariableType),
+    Whitespace
 }
 
 pub enum Keyword {
+    // Variable related
+    Var,
+    Const,
 
+    When,
+
+    Previous,
+    Next,
+    Current,
+
+    Delete,
+
+    // Structure
+    If,
+    Else,
+
+    Async,
+    Function,
+    Noop,
+
+    Class,  // can be `class` or `className`
+
+    Reverse,
 }
